0f6f3b789331600b0ec8aee9b310645145f123c0
Harsh Shandilya <me@msfjarvis.dev>
2023-01-12 16:39:49 +0530

    .

--- .github/check-pr-base.js
+++ .github/check-pr-base.js
@@ -1,21 +1,8 @@
 module.exports = async ({github, context}) => {
-  const pr = await github.rest.pulls.get({
-    owner: context.payload.repository.owner.login,
-    repo: context.payload.repository.name,
-    pull_number: context.payload.number,
-  });
-
-  console.log(Object.getOwnPropertyNames(pr));
-  const shouldWarn = pr.head.ref != "staging" && pr.base.ref == "main";
-
-  if (shouldWarn) {
     octokit.rest.issues.createComment({
       owner: context.payload.repository.owner.login,
       repo: context.payload.repository.name,
       issue_number: context.payload.number,
       body: `This PR is targeting the 'main' branch but did not originate from 'staging', please make sure this intentional`,
     });
-  }
-
-  return shouldWarn
 }
